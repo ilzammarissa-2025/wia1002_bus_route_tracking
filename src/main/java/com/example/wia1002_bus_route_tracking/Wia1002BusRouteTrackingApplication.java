815a9e370c1e8e94fba4896f55b860bd22c1e401
123EFD <erchinqq06@gmail.com>
2026-04-26 13:58:43 +0800

    first commit

---
+++ src/main/java/com/example/wia1002_bus_route_tracking/Wia1002BusRouteTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.wia1002_bus_route_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wia1002BusRouteTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wia1002BusRouteTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wia1002_bus_route_tracking/Wia1002BusRouteTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wia1002_bus_route_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wia1002BusRouteTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
