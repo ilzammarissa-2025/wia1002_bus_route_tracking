fc5de952bc01b8e742282e0b85573d5456a88ecf
123EFD <erchinqq06@gmail.com>
2026-05-05 23:02:20 +0800

    Implement RouteController, RouteService, and RouteStop entity; update application properties and add WebSocketConfig

--- src/main/java/com/example/wia1002_bus_route_tracking/controller/RouteController.java
+++ src/main/java/com/example/wia1002_bus_route_tracking/controller/RouteController.java
@@ -1,5 +1,28 @@
 package com.example.wia1002_bus_route_tracking.controller;
 
+import java.util.LinkedList;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.wia1002_bus_route_tracking.model.entity.BusStop;
+import com.example.wia1002_bus_route_tracking.service.RouteService;
+
+//@RestController annotation auto return JSON for Flutter
+
+@RestController
+@RequestMapping("/api/routes")
+@CrossOrigin(origins = "*") // Allow CORS for all origins (for development only, consider restricting in production)
+
 public class RouteController {
-    
+    private final RouteService routeService;
+
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    //GET : Example: http://localhost:8080/api/routes/T818/stops
+    @GetMapping("/{routeId}/stops")
+    public LinkedList<BusStop> getRouteStops(@PathVariable String routeId) {
+        return routeService.getRouteStops(routeId);
+    }
 }
--- src/main/java/com/example/wia1002_bus_route_tracking/repository/BusStopRepository.java
+++ src/main/java/com/example/wia1002_bus_route_tracking/repository/BusStopRepository.java
@@ -11,11 +11,26 @@ import java.util.List;
 public interface BusStopRepository extends JpaRepository<BusStop, String> {
 
     //REPOSITORY LAYER : interface for CRUD operations and custom PostGIS queries (similar to SQL)
-    // purpose : find stops within a certain radius (in meters)
+    /* 
+    purpose : find stops within a certain radius (in meters) ~ find bus near me
+    nativeQuery = true : indicates that the query is a native SQL query, not JPQL
+    CAST(...AS geography): tells the database to calculate distance in meters on globe
+    concept: create a circle near user and return points in the bus_stops table fall within
+    */ 
+
     @Query(value = "SELECT * FROM bus_stops b " +
                    "WHERE ST_DWithin(b.location, CAST(ST_SetSRID(ST_MakePoint(:lon, :lat), 4326) AS geography), :radiusMeters)", 
            nativeQuery = true)
     List<BusStop> findStopsWithinRadius(@Param("lat") double lat, 
                                         @Param("lon") double lon, 
                                         @Param("radiusMeters") double radiusMeters);
+
+    
+    //use JPQL for BusStop and RouteStop, loop up mapping table to find all IDs associated with a route, then fetches the full BusStop obects for those IDs
+    //use when student clicks specific bus route and see every stop
+    //Using a JOIN guarantees the outer BusStop list is sorted by the RouteStop sequence.
+    @Query("SELECT s FROM BusStop s JOIN RouteStop rs ON s.stopId = rs.stopId " +
+           "WHERE rs.routeId = :routeId ORDER BY rs.stopSequence ASC")
+    List<BusStop> findStopsByRouteId(@Param("routeId") String routeId);                                    
+    
 }
